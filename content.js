989ef2f5be2c59b166459df197ebebc6a55d3b74
lechiffre4 <toxcoa@gmail.com>
2023-02-26 17:36:09 +0100

    test

--- content.js
+++ content.js
@@ -1,4 +1,21 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+
+//fonction qui récupère les textes d'une page et met la moitié de chaque mot en gras
+
+function boldHalfText() {
+
+var text = document.body.innerText;
+
+var words = text.split(" ");
+for (var i = 0; i < words.length; i++) {
+
+var halfWord = words[i].substring(0, words[i].length / 2);
+words[i] = halfWord.bold() + words[i].substring(words[i].length / 2);
+
 }
+
+document.body.innerHTML = words.join(" ");
+
+}
+
+//appel de la fonction
+boldHalfText();
